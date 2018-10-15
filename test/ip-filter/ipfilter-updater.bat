12204127f3e526b80b237a0d649daae776313a5f
CHEF-KOCH <CHEF-KOCH@protonmail.com>
2018-10-15 22:46:41 +0200

    Update ipfilter-updater.bat

--- test/ip-filter/ipfilter-updater.bat
+++ test/ip-filter/ipfilter-updater.bat
@@ -3,5 +3,5 @@ wget http://upd.emule-security.org/ipfilter.zip
 cd upd.emule-security.org
 c:\util\unzip.exe ipfilter.zip
 ren guarding.p2p ipfilter.dat
-xcopy /d /y ipfilter.dat c:\Users\njamnjam\AppData\Roaming\uTorrent\ipfilter.dat
+xcopy /d /y ipfilter.dat %appdata%/qBittorrent/ipfilter.dat
 del ipfilter.dat
